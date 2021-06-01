c96cbecfa4bfbc57671649449a7a78bf1c1bc4f2
Clayton Davidson <clayton.davidson847@topper.wku.edu>
2021-06-01 15:00:55 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,131 @@
+import {
+  moveMouse,
+  mouseClick,
+  getPixelColor,
+  screen,
+  keyToggle,
+} from "robotjs";
+
+const main = () => {
+  console.log("A simple woodcutting bot made by Clayton Davidson");
+  console.log("Starting...");
+  sleep(4000);
+
+  while (true) {
+    const tree = findTree();
+    if (!tree) {
+      rotateCamera();
+      continue;
+    }
+
+    moveMouse(tree.x, tree.y);
+    mouseClick();
+    sleep(3000);
+
+    dropLogs();
+  }
+};
+
+const dropLogs = () => {
+  const inventory_x = 1862;
+  const inventory_y = 836;
+  const inventory_log_color = "6f512c";
+
+  let pixel_color = getPixelColor(inventory_x, inventory_y);
+
+  let wait_cycles = 0;
+  const max_wait_cycles = 9;
+  while (pixel_color != inventory_log_color && wait_cycles < max_wait_cycles) {
+    sleep(1000);
+    pixel_color = getPixelColor(inventory_x, inventory_y);
+    wait_cycles++;
+  }
+
+  if (pixel_color == inventory_log_color) {
+    moveMouse(inventory_x, inventory_y);
+    mouseClick("right");
+    sleep(300);
+    moveMouse(inventory_x, inventory_y + 40);
+    mouseClick();
+    sleep(1000);
+  }
+};
+
+const findTree = () => {
+  const x = 300,
+    y = 300,
+    width = 1300,
+    height = 700;
+  const img = screen.capture(x, y, width, height);
+  const tree_colors = [
+    "71532d",
+    "6c502b",
+    "261c05",
+    "3d2d18",
+    "171007",
+    "3F2F19",
+  ];
+
+  for (let i = 0; i < 500; i++) {
+    const random_x = getRandomInt(0, width - 1);
+    const random_y = getRandomInt(0, height - 1);
+    const sample_color = img.colorAt(random_x, random_y);
+
+    if (tree_colors.includes(sample_color)) {
+      const screen_x = random_x + x;
+      const screen_y = random_y + y;
+
+      if (confirmTree(screen_x, screen_y)) {
+        console.log(
+          "Found a tree at: " +
+            screen_x +
+            ", " +
+            screen_y +
+            " color " +
+            sample_color
+        );
+        return { x: screen_x, y: screen_y };
+      } else {
+        console.log(
+          "Unconfirmed tree at: " +
+            screen_x +
+            ", " +
+            screen_y +
+            " color " +
+            sample_color
+        );
+      }
+    }
+  }
+
+  return false;
+};
+
+const rotateCamera = () => {
+  console.log("Rotating camera");
+  keyToggle("right", "down");
+  sleep(1000);
+  keyToggle("right", "up");
+};
+
+const confirmTree = (screen_x: number, screen_y: number) => {
+  moveMouse(screen_x, screen_y);
+  sleep(300);
+
+  const check_x = 156;
+  const check_y = 66;
+  const pixel_color = getPixelColor(check_x, check_y);
+
+  return pixel_color == "00ffff";
+};
+
+const sleep = (ms: number) =>
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
+
+const getRandomInt = (min: number, max: number): number => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+main();
